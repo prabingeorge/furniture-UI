ea8aad7f2962b6a390c060cfbb3c5c24aa4c7267
prabingeorge <prabingeorge@gmail.com>
2026-01-03 13:56:31 +0530

    Root code changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/unauthorized" element={<Unauthorized />} />
