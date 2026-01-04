3ce04ccb3ea4f6027e60e0dc57f2b3f76cd0e79e
prabingeorge <prabingeorge@gmail.com>
2026-01-04 12:55:30 +0530

    Added public images

--- src/components/Images/Images.jsx
+++ src/components/Images/Images.jsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom";
 import './index.css';
 
 // const baseUrl = `http://localhost:5173/src/assets/images/`;
-const baseUrl = `src/assets/images/`;
+// const baseUrl = `https://furniture-ui-2qka.onrender.com/public/images`;
+const baseUrl = `/images/`;
 
 const Images = ({fileName, categoryId, id, path, cssClass}) => {
     const url = baseUrl + path + '/' + fileName;
